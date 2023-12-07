a5409cbf0ca74cc0a2e6fc1d4d5266efc2ca9470
Owdea <ondrejpel@seznam.cz>
2023-12-07 15:02:57 +0100

    Map with init position on Argo22 with marker

--- src/pages/MapTestPage.tsx
+++ src/pages/MapTestPage.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {View, StyleSheet, Button} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const styles = StyleSheet.create({
   container: {
@@ -22,8 +22,8 @@ const styles = StyleSheet.create({
 });
 
 let region = {
-  latitude: 37.78825,
-  longitude: -122.4324,
+  latitude: 48.97406661069083,
+  longitude: 14.472992967998712,
   latitudeDelta: 0.015,
   longitudeDelta: 0.0121,
 };
@@ -35,8 +35,16 @@ function MapTestPage() {
         <MapView
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.mapView}
-          region={region}
-        />
+          region={region}>
+          <Marker
+            coordinate={{
+              latitude: 48.97406661069083,
+              longitude: 14.472992967998712,
+            }}
+            title={'ARGO22'}
+            description={'office'}
+          />
+        </MapView>
       </View>
       <View style={styles.buttons}>
         <Button title={'button1'} />
