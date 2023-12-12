f8bd0ab8cc145387c364c8f1bb7b116aff34b249
Owdea <ondrejpel@seznam.cz>
2023-12-12 03:27:13 +0100

    Deleted useless text and code, added MapTestPage as default page

--- src/pages/MapTestPage.tsx
+++ src/pages/MapTestPage.tsx
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {View, StyleSheet, Button} from 'react-native';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {useRef} from 'react'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const styles = StyleSheet.create({
   container: {
@@ -20,14 +21,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
 function MapTestPage() {
-  // @ts-ignore
+  const DEFAULT_PADDING = {
+    top: 20000,
+    right: 20000,
+    bottom: 20000,
+    left: 20000,
+  };
+
+  const mapViewRef = useRef<MapView>(null); //vytvoření reference na MapView
+  const argoCoords = {
+    latitude: 48.97406661069083,
+    longitude: 14.472992967998712,
+  };
+  async function fitARGOMarker() {
+    mapViewRef.current?.fitToCoordinates([argoCoords], {
+      edgePadding: DEFAULT_PADDING,
+    });
+  }
   return (
     <View style={styles.container}>
       <View style={styles.map}>
         <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          ref={mapViewRef} //přiřazení reference
+          provider={PROVIDER_GOOGLE}
           style={styles.mapView}
           initialRegion={{
             latitude: 48.97406661069083,
@@ -46,7 +63,7 @@ function MapTestPage() {
         </MapView>
       </View>
       <View style={styles.buttons}>
-        <Button title={'button1'} />
+        <Button title={'Locate ARGO22'} onPress={fitARGOMarker} />
         <Button title={'Button2'} />
       </View>
     </View>
